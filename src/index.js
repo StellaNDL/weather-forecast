c3b40d6c006ca2346e8941dbdd7f8d35014bd917
StellaNDL <b.sndlakuse@gmail.com>
2024-04-08 09:57:37 +0200

    Updated the js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = response.data.temperature.current;
+}
+
+function searchCity(city) {
+let apiKey = "c2fa2f0a85c4ebtf3304fbofa4a162d0";
+let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
+axios.get(apiUrl).then(refreshWeather)
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 
